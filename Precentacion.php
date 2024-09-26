306c5438ba3f3bfd5efdf14837305c518f50ca6b
RojoR <drojo6507@gmail.com>
2024-09-25 20:02:12 -0600

    Formulario Para el regitro de nuevas precentaciones de los productos 1.1

--- Precentacion.php
+++ Precentacion.php
@@ -6,7 +6,7 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registrar Presentacion</title>
+    <title>Registrar Nueva Presentacion</title>
     <link rel="stylesheet" href="estilos/categoria1.css">
 </head>
 <body>
