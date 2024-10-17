069040ed61de563b1704d5947cf849a42826be8f
RojoR <drojo6507@gmail.com>
2024-10-17 11:09:50 -0600

    Consulta para Eliminacion logica presentacion 1.2

--- elim-pres.php
+++ elim-pres.php
@@ -1,6 +1,6 @@
 <?php
 include('conexion/conexion.php');
- //eliminacion logica de una presentacion 
+ //eliminacion logica de una presentacion y darlo de baja cambiando su estado a 0
 $idProd = $_GET['id'];
 $sql = "UPDATE presentacion SET estatus = '0'
     WHERE idPresentacion = '$idProd'";
