0715a3cb596b6b6d303ac9d1d90e0d8eb3ce9290
RojoR <drojo6507@gmail.com>
2024-10-19 11:29:14 -0600

    Consulta para Eliminacion de las categorias del catalogo

---
+++ Elimina-cat.php
@@ -0,0 +1,13 @@
+<?php
+include('conexion/conexion.php');
+ 
+$idProd = $_GET['id'];
+$sql = "UPDATE categoria SET estatus = '0'
+    WHERE idCategoria = '$idProd'";
+
+if(mysqli_query($conexion,$sql)){
+    header("location:Categoria.php");
+}else{
+    echo "problemas al Eliminar la Categoria, consulte al administrador";
+}
+?>
