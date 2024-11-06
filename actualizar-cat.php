723f2589ad8eb1c356d494a23856c150a5dd8f9e
RojoR <drojo6507@gmail.com>
2024-11-06 11:55:46 -0600

    Archivo php para la insercion de las acualizaciones en las categorias 1.1

---
+++ actualizar-cat.php
@@ -0,0 +1,23 @@
+<?php
+include('conexion/conexion.php');
+$cat = $_POST['txtIDM'];
+$descripcionC = $_POST['txtMar'];
+$estatus=$_POST['txtestatus'];
+
+
+$consulta="Update categoria
+SET idCategoria = '$cat',
+descripcionCat = '$descripcionC',
+estatus='$estatus'
+WHERE idCategoria = '$cat'";
+
+if(mysqli_query($conexion,$consulta)){
+    header("location:Categoria.php");
+
+}else{
+    echo "Problemas al actualizar la Categoria ,Consulte al administrador";
+}
+
+ mysqli_close($conexion);
+
+?>
