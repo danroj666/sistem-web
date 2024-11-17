23efd62a04b1e91928eadfe16e4edb48d85f3065
RojoR <drojo6507@gmail.com>
2024-11-16 18:52:12 -0600

    Archivo php para la eliminacion de usuarios 1.1

---
+++ eliminar-user.php
@@ -0,0 +1,12 @@
+<?php
+include('conexion/conexion.php');
+ 
+$idProd = $_GET['id'];
+$sql = "UPDATE usuario SET estatus = '0'
+    WHERE idUsuario = '$idProd'";
+
+if(mysqli_query($conexion,$sql)){
+    header("location:consultar-Usuario.php");
+}else{
+    echo "problemas al Eliminar Producto, consulte al administrador";
+}
