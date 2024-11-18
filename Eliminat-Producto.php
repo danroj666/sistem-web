68f3348b08811314873d53efc2340c64ab602b9b
RojoR <drojo6507@gmail.com>
2024-11-17 22:43:09 -0600

    Archivo php para la eliminacion de productos final

--- Eliminat-Producto.php
+++ Eliminat-Producto.php
@@ -1,9 +1,13 @@
 <?php
 include('conexion/conexion.php');
- 
+ //eliminacion logica de productod
 $idProd = $_GET['id'];
 $sql = "UPDATE producto SET estatus = '0'
     WHERE clvProd = '$idProd'";
 
-
+if(mysqli_query($conexion,$sql)){
+    header("location:consulta-Productos.php");
+}else{
+    echo "problemas al Eliminar Producto, consulte al administrador";
+}
 ?>
